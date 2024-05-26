5967619da864c43a466ab06d583dff9ed544782e
Takumikimura1899 <kimura141899@gmail.com>
2024-05-26 14:29:22 +0900

    ダークモード改修 tailwind,shadcn/ui導入 スタイリング見直し

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-import mdx from '@astrojs/mdx'
-import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap'
-import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [
     mdx(),
     sitemap(),
     icon(),
     partytown({
       config: {
-        forward: ['dataLayer.push'],
+        forward: ["dataLayer.push"],
       },
     }),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
   ],
-})
+});
