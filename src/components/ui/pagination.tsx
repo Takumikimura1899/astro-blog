e401a1203cccc0bcf2ce2265c93ef4c07cc52c4e
Takumikimura1899 <kimura141899@gmail.com>
2024-09-23 12:11:11 +0900

    fix: lintError

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,8 +1,8 @@
+import type { ButtonProps } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react'
 import * as React from 'react'
-
-import { ButtonProps, buttonVariants } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
 
 const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
   <nav
